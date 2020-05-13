9f23ce616f4f373670492d9a16ed9849423b7347
angieramirez1 <patataramirez13@gmail.com>
2020-05-12 23:14:46 -0500

    Home page

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/home', function (request, response) {
+    response.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+
 app.listen(3000, function () {
     console.log('servidor iniciado');
   });
