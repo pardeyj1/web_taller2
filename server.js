493708444ec08ca6c52edc66ab29086e5d804b77
angieramirez1 <patataramirez13@gmail.com>
2020-05-12 23:08:29 -0500

    Server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const path = require('path');
+
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.listen(3000, function () {
+    console.log('servidor iniciado');
+  });
